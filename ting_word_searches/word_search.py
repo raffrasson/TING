68058ac8ffac59ec89f02e90c2711deaed0eacc8
raffrasson <raf.frasson@gmail.com>
2022-10-28 11:28:55 -0300

    feat: partially completes req. 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -5,14 +5,17 @@ def exists_word(word, instance):
         for line in item["linhas_do_arquivo"]:
             if word.lower() in line.lower():
                 lines_found.append(
-                    {"linha": item["linhas_do_arquivo"].index(line), "conteúdo": line}
+                    {
+                        "linha": item["linhas_do_arquivo"].index(line),
+                        "conteúdo": line,
+                    }
                 )
                 print("lines_found")
         if len(lines_found) > 0:
             formatted_data = {
                 "palavra": word,
                 "arquivo": item["nome_do_arquivo"],
-                "ocorrencias": lines_found
+                "ocorrencias": lines_found,
             }
             list.append(formatted_data)
     return list
