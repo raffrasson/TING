fd1ab9388f14c30bea93c658d2343ce7878fa88f
raffrasson <raf.frasson@gmail.com>
2022-10-27 22:12:54 -0300

    feat: completes req. 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt") is False:
+        print("Formato inválido", file=sys.stderr)
+    else:
+        try:
+            with open(path_file) as file:
+                lines = file.readlines()
+                formatted_lines = []
+                for line in lines:
+                    line = line.strip("\n")
+                    formatted_lines.append(line)
+                return formatted_lines
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
