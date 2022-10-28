4156e558545f97909695d110b7085eacb8164674
raffrasson <raf.frasson@gmail.com>
2022-10-28 07:21:41 -0300

    feat: completes req. 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    queue = txt_importer(path_file)
+    formatted_output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(queue),
+        "linhas_do_arquivo": queue,
+    }
+    for item in instance._data:
+        if path_file == item["nome_do_arquivo"]:
+            return None
+    instance.enqueue(formatted_output)
+    print(formatted_output)
 
 
 def remove(instance):
